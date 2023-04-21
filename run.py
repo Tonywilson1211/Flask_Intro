a51dd0a68766dd2c345b9997514a81d4946f072e
Tonywilson1211 <>
2023-04-21 14:20:52 +0000

    add base file

--- run.py
+++ run.py
@@ -1,20 +1,37 @@
 import os
-from flask import Flask #import flask class - capital letter indicates a class and is important to remember
+# import flask class - capital letter indicates a class and is important to remember
+from flask import Flask, render_template
 
 
-app = Flask(__name__)  
-#create instance and store in variable called app. 
-#The first argument of the Flask class, is the name of the application's module - our package.
-#Since we're just using a single module, we can use __name__ which is a built-in Python variable.
-#Flask needs this so that it knows where to look for templates and static files.
+app = Flask(__name__)
+# create instance and store in variable called app.
+# The first argument of the Flask class, is the name of the application's module - our package.
+# Since we're just using a single module, we can use __name__ which is a built-in Python variable.
+# Flask needs this so that it knows where to look for templates and static files.
 
-@app.route("/") #decorator starts with @ or pie notation. 
+
+@app.route("/") #  decorator starts with @ or pie notation.
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
-if__name__ =="__main__":#name of default module in python
-app.run(
-    host=os.environ.get("IP", "0.0.0.0"), #Using the os module from the python library to get the 'IP' environment variable if it exists, but set a default value if it's not found.
-    port=int(os.environ.get("PORT", "5000")), #It will be the same with 'PORT', but this time, we're casting it as an integer, and I will set that default to "5000", which is a common port used by Flask.
-    debug=True #CHANGE TO FALSE BEFORE SUBMITTING PROJECT - IF NOT, IT CREATES A SECURITY FLAW
-)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
+if __name__ == "__main__":  # name of default module in python
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),  # Using the os module from the python library to get the 'IP' environment variable if it exists, but set a default value if it's not found.
+        port=int(os.environ.get("PORT", "5000")),  # It will be the same with 'PORT', but this time, we're casting it as an integer, and I will set that default to "5000", which is a common port used by Flask.
+        debug=True)  # CHANGE TO FALSE BEFORE SUBMITTING PROJECT - IF NOT, IT CREATES A SECURITY FLAW

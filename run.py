5bf50c44ba3827ae2a47b8575b2e66cd61fc8cc4
Tonywilson1211 <>
2023-04-25 18:49:34 +0000

    debug to false

--- run.py
+++ run.py
@@ -53,4 +53,4 @@ if __name__ == "__main__":  # name of default module in python
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),  # Using the os module from the python library to get the 'IP' environment variable if it exists, but set a default value if it's not found.
         port=int(os.environ.get("PORT", "5000")),  # It will be the same with 'PORT', but this time, we're casting it as an integer, and I will set that default to "5000", which is a common port used by Flask.
-        debug=True)  # CHANGE TO FALSE BEFORE SUBMITTING PROJECT - IF NOT, IT CREATES A SECURITY FLAW
+        debug=False)  # CHANGE TO FALSE BEFORE SUBMITTING PROJECT - IF NOT, IT CREATES A SECURITY FLAW
